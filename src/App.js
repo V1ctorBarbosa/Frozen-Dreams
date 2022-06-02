ec454c8b01e68e20d32788d8bbc8dcb09dc66b56
Victor Barbosa <vicfer2708@gmail.com>
2022-06-01 22:50:41 -0300

    frozen-dreams

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Footer from './components/Footer/Footer';
 
 function App() {
 	return (
-		<Router basename={process.env.PUBLIC_URL}>
+		<Router>
 			<GlobalStyle />
 			<Navbar />
 			<Routes>
