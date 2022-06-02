847e6a7d5ef0d422feff85ca6f5ea5038f55c77b
Victor Barbosa <vicfer2708@gmail.com>
2022-06-01 23:19:01 -0300

    frozen dreams

--- src/data/FeaturesData.jsx
+++ src/data/FeaturesData.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { IoMdIceCream } from "react-icons/io";
+import { MdWash, MdDeliveryDining } from "react-icons/md";
 import { BiSupport, BiDollar, BiCool } from 'react-icons/bi';
 
 const iconStyle = (Icon) => <Icon size="3rem" color="#E91601" />;
@@ -12,6 +13,12 @@ export const featuresData = [
 		imgClass: 'one',
 	},
 	{
+		name: 'Clean Environment',
+		description: 'Kitchen and lounge cleaned daily',
+		icon: iconStyle(MdWash),
+		imgClass: 'two',
+	},
+	{
 		name: '24/7 Support',
 		description: 'Our team is available at all times in case you need us',
 		icon: iconStyle(BiSupport),
@@ -22,6 +29,12 @@ export const featuresData = [
 		description: 'We offer the highest value/cost ratio',
 		icon: iconStyle(BiDollar),
 		imgClass: 'four',
+	},
+	{
+		name: 'Delivery',
+		description: 'The fastest delivery in town',
+		icon: iconStyle(MdDeliveryDining),
+		imgClass: 'five',
 	},
 	{
 		name: 'Family Friendly',
